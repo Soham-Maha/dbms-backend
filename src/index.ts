aea5a2279ef9bca6da66ce3c580c282cc3412fd1
Soham Mahapatra <soham.mahapatra2005@gmail.com>
2025-10-13 23:58:49 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = 8080;
+
+app.get("/ping", (_, res) => {
+  res.json({ status: "ok", message: "pong" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
